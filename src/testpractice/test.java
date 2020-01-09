f8d0bc7889d8988c70506d6425a29ccee773803f
captor1004 <captor1004@gmail.com>
2020-01-10 08:01:24 +0900

    First commit test 첫번쨰 커밋

---
+++ src/testpractice/test.java
@@ -0,0 +1,42 @@
+package testpractice;
+
+public class test {
+
+	private String student;
+	private String parent;
+	private String teacher;
+	
+	
+	
+	
+	public test(String student, String parent) {
+		this(student, parent);
+	}
+	
+	
+	public test(String student, String parent, String teacher) {
+		this.student = student;
+		this.parent = parent;
+		this.teacher = teacher;
+	}
+	public String getStudent() {
+		return student;
+	}
+	public void setStudent(String student) {
+		this.student = student;
+	}
+	public String getParent() {
+		return parent;
+	}
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	
+	
+}
---
+++ src/testpractice/test2.java
@@ -0,0 +1,21 @@
+package testpractice;
+
+public class test2 {
+
+	
+	public class Parent {
+	int age;	
+	String name;
+	}
+	
+	public class Child extends Parent{
+		
+	}
+	
+	Parent parent = new Child();
+	Child b = new Child();
+	
+	
+//	b= parent;
+	
+}
